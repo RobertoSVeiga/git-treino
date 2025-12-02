94583b3fb674aff4f7373101ee8574aec9cf8f94
Roberto Veiga <roberto.veiga0705@gmail.com>
2025-12-02 09:52:54 -0300

    Acrescentei a funcao para_minuscula(texto)

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 
 def soma(a,b):
     return a + b
+
+
+
+def para_minuscula(texto):
+    """
+    Recebe uma string e retorna em letras minúsculas.
+    """
+    return texto.lower()
+
+# Exemplo de uso:
+print(para_minuscula("Olá Mundo!"))  # saída: "olá mundo!"
+
