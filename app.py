e2bf298c5949dca9170d837a9f396e49f550065a
Roberto Veiga <roberto.veiga0705@gmail.com>
2025-12-02 09:18:16 -0300

    Acrescentei a função fatorial

--- app.py
+++ app.py
@@ -7,6 +7,20 @@ def exibir_boas_vindas():
     nome = input("Digite seu nome: ")
     print(f"Olá, {nome}! Vamos somar dois números.")
 
+def fatorial(n):
+    if n < 0:
+        raise ValueError("O fatorial não está definido para números negativos.")
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        resultado = 1
+        for i in range(2, n + 1):
+            resultado *= i
+        return resultado
+
+# Exemplo de uso:
+print(fatorial(5))  # Saída: 120
+
 
 if __name__ == "__main__":
     exibir_boas_vindas()
